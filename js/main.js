c5594cb8b81635d9ca2b66a0d1109f6346fbd4ee
ALAM-97 <alexamara97@gmail.com>
2021-10-22 15:45:19 +0200

    Error

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
 const app = new Vue({
     el: '#app',
     data: {
-        students: []
+        albums: [],
+        genres: [],
+        genreSelected: ""
     },
     created() {
         axios.get('http://localhost/php-ajax-dischi/api/index.php')
             .then((resp) => {
                 // handle success
-                this.students = resp.data;
+                this.albums = resp.data;
             })
+    },
+    methods: {
+        genresFind() {
+            this.albums.forEach(genre => {
+                
+                console.log(genre)
+            });
+        }
+    //     selectGenre() {
+    //         axios.get('http://localhost/php-ajax-dischi/api/index.php' ,{
+    //             params: {
+    //                 genre = this.genres
+    //             }
+    //         }) 
+    //         .then((resp) => {
+    //             // handle success
+    //             console.log(resp.data.genre);
+    //         })
+    //     }
     }
 })
